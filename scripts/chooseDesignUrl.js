571c9e0ee4bcbe6787d18bb5dfbe7362844c7c8d
Scott Vorthmann <scott@vorthmann.org>
2022-02-06 18:15:21 -0600

    Create chooseDesignUrl.js
    Intended for use in a GitHub Action, selecting from a predetermined list of page URLs to tweet.

---
+++ scripts/chooseDesignUrl.js
@@ -0,0 +1,22 @@
+
+const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+
+const days = Math.floor( Date.now() / MILLISECONDS_PER_DAY );
+
+const daysSinceStart = days - 19030; // 2022-02-06
+
+const tweetInterval = 3; // days
+
+const tweetsSinceStart = Math.floor( daysSinceStart / tweetInterval );
+
+const urls = [
+  "https://vorth.github.io/vzome-sharing/2022/02/06/Yellow-Stretch-120cell-13-23-08.html",
+  "https://vorth.github.io/vzome-sharing/2022/01/15/compound-5-tetra-09-07-31.html",
+  "https://vorth.github.io/vzome-sharing/2021/12/07/Keplers-Kosmos-17-39-10.html",
+  "https://vorth.github.io/vzome-sharing/2021/12/04/no-twist-RT-12-09-21.html",
+  "https://vorth.github.io/vzome-sharing/2021/12/04/4d-buckyball-15-24-24.html"
+];
+
+const index = tweetsSinceStart % urls.length;
+
+console.log( urls[ index ] );

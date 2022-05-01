884b18fcf86920bec562381cd1b23ce93ff2f615
Scott Vorthmann <scott@vorthmann.org>
2022-05-01 15:36:13 -0700

    Update chooseDesignUrl.js

--- scripts/chooseDesignUrl.js
+++ scripts/chooseDesignUrl.js
@@ -51,7 +51,8 @@ const urls = [
   "https://vorth.github.io/vzome-sharing/2022/04/12/10-G00-green-struts-21-58-52.html",
   "https://vorth.github.io/vzome-sharing/2022/04/16/Rashmi-tiling-unit-08-18-21.html",
   "https://john-kostick.github.io/vzome-sharing/2022/04/27/Six-block-TO-07-49-35.html",
-  "https://john-kostick.github.io/vzome-sharing/2022/04/27/6-axis-16-42-25.html"
+  "https://john-kostick.github.io/vzome-sharing/2022/04/27/6-axis-16-42-25.html",
+  "https://john-kostick.github.io/vzome-sharing/2022/05/01/Orange-black-purple-snub-dodecahedron-17-12-45.html"
 ];
 
 const index = tweetsSinceStart % urls.length;

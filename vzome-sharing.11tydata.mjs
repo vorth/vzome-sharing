34e40ac3002f1bc5bb9ffa7c8ef58d9ce04359d2
Scott Vorthmann <scott@vorthmann.org>
2025-11-18 07:38:50 -0800

    Prepared for 11ty build in vzome/vzome
    With these changes, this repo can be checked out inside a
    working copy of `vzome/vzome`, and this content will be built
    into the website using 11ty.
    
    Changes:
    - added 11ty config files, to match the Jekyll post slugs, and
        correctly add posts to a collection
    - removed problematic "{{{{" comments in a few HTML files

---
+++ vzome-sharing.11tydata.mjs
@@ -0,0 +1,30 @@
+
+export default {
+	permalink: function ( data ) {
+    const { layout, tags, title, description, image, page } = data;
+    const { inputPath, fileSlug, date, filePathStem, outputFileExtension } = page;
+    let result = filePathStem .replace( '/vzome-sharing/', '/geometry/' ) + '.' + outputFileExtension;
+    if ( tags && tags .includes( 'posts' ) ) {
+      // console.dir( { layout, tags, title, page: { inputPath, fileSlug, date, filePathStem, outputFileExtension } } );
+      result = result .replace( '_posts', page.date .toISOString() .slice( 0, 10 ) .replace( /-/g, '/' ) );
+    }
+    console.log( result );
+    return result;
+	},
+};
+
+/*
+{
+  layout: 'vzome',
+  tags: [ 'posts' ],
+  title: 'Snub Dodecahedron Zometool-like System',
+  page: {
+    inputPath: './vzome-sharing/_posts/2021-12-05-snub-dodec-zome-system-21-44-04.md',
+    fileSlug: 'snub-dodec-zome-system-21-44-04',
+    date: 2021-12-05T00:00:00.000Z,
+    filePathStem: '/vzome-sharing/_posts/snub-dodec-zome-system-21-44-04',
+    outputFileExtension: 'html'
+  }
+}
+/geometry/snub-dodec-zome-system-21-44-04.html
+*/
